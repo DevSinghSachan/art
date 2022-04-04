912a5541a928e56972933ef6b674b5ed5e77a185
Devendra Singh Sachan <sachan.devendra@gmail.com>
2022-04-04 13:26:39 -0400

    correct computation of loss

--- tasks/openqa/e2eqa/train_e2eqa.py
+++ tasks/openqa/e2eqa/train_e2eqa.py
@@ -169,11 +169,11 @@ def _cross_entropy_forward_step(batch, model):
     if args.update_retriever:
         t0_tokenizer = get_t0_tokenizer()
         eos_id = t0_tokenizer.eos_token_id
-        retriever_loss, weighted_ru, null_block_loss = get_loss_and_retriever_utility(lm_logits_one_context,
-                                                                                      topk_log_probs,
-                                                                                      query_ids_t0,
-                                                                                      query_mask_t0,
-                                                                                      eos_id)
+        retriever_loss = get_loss_and_retriever_utility(lm_logits_one_context,
+                                                        topk_log_probs,
+                                                        query_ids_t0,
+                                                        query_mask_t0,
+                                                        eos_id)
     net_loss = retriever_loss
     reduced_loss = reduce_losses([retriever_loss])
 
